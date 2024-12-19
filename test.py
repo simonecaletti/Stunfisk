4b5ef47ec3a2f6c6cc96336d8e484eba9db2c8d4
simonecaletti <iscrizioni.simone@gmail.com>
2024-12-19 10:46:13 +0100

    added time to test

--- test.py
+++ test.py
@@ -3,10 +3,13 @@
 from structure.pokemon import Pokemon
 from structure.damage import damage, damage_list
 import statistic.calculator as calc
+import time
 
 move = "Aerial Ace"
 pkm1 = Pokemon("Chikorita", 50, [move], None, None, [4,252,0,0,0,252], [31,31,31,31,31,31], "adamant", "fire", statsmodifier=[3,0,0,0,0])
-pkm2 = Pokemon("Charmander", 50, [move], None, None, [252,0,252,0,4,0], [31,31,31,31,31,31], "serious", "fire", statsmodifier=[0,1,0,0,0])
+pkm2 = Pokemon("Charmander", 50, [move], None, None, [252,0,252,0,4,0], [31,31,31,31,31,31], "serious", "fire", statsmodifier=[0,0,0,0,0])
+
+start_time = time.time()
 
 #check stats calc
 print("{}, lv ".format(pkm1.specie), pkm1.lv)
@@ -36,12 +39,14 @@ print("")
 
 #check calculator
 #print("{} HP before attack: ".format(pkm2.specie), pkm2.currentHP)
-#calc.apply_dmg(pkm2, dmg)
+#calc.apply_dmg(pkm2, dmg) #apply_dmg modifies the NHKO estimate!!!
 #print("{} HP after attack: ".format(pkm2.specie), pkm2.currentHP)
 n = calc.count_nhko(pkm2.currentHP, dmgmax)
 print("This attack is a potential {}HKO on the target!".format(n))
 prob = calc.get_probability_nhko(pkm2.currentHP, dmgall)*100
 print("The probability to get a {}HKO is: {}%".format(n, prob))
+print("")
 
-
+#execution time
+print("Program executed in {} seconds".format(time.time() - start_time))
 
